cc7cb622ada0557232718d89c5b2d12681f0f31e
Tanner Martin <79233634+BinderFluids@users.noreply.github.com>
2026-02-15 20:52:57 -0700

    Board Node Instantiation

--- Assets/Scripts/Board/Board.cs
+++ Assets/Scripts/Board/Board.cs
@@ -3,8 +3,12 @@ using UnityEngine;
 
 public class Board : MonoBehaviour
 {
+    [SerializeField] private BoardNode boardNodePrefab;
+    [SerializeField] private Transform boardNodeContainer;
+    [SerializeField] private Transform boardAnchor;
+    [SerializeField] private float gridCellSize;
     [Min(1), SerializeField] private int gridWidth;
-    [Min(1), SerializeField] private int gridHeight; 
+    [Min(1), SerializeField] private int gridHeight;
     
     private Grid<BoardNode> grid;
     public Grid<BoardNode> Grid => grid;
@@ -12,5 +16,28 @@ public class Board : MonoBehaviour
     private void Awake()
     {
         grid = new Grid<BoardNode>(gridWidth, gridHeight);
+        int colorIndex = 0;
+        Color[] colors =
+        {
+            Color.red,
+            Color.blue
+        };
+        
+        for (int y = 0; y < gridWidth; y++)
+        {
+            for (int x = 0; x < gridHeight; x++)
+            {
+                BoardNode node = Instantiate(boardNodePrefab, transform);
+                node.Init(colors[colorIndex % 2], gridCellSize);
+                
+                Vector3 boardOffset = new Vector3(x * gridCellSize, 0, y * gridCellSize);
+                Vector3 squareOffset = new Vector3(gridCellSize / 2, 0, gridCellSize / 2);
+                node.transform.localPosition = boardAnchor.position + boardOffset + squareOffset;
+                node.transform.SetParent(boardNodeContainer);
+                
+                colorIndex++;
+            }
+            colorIndex++;
+        }
     }
 }
--- Assets/Scripts/Board/BoardNode.cs
+++ Assets/Scripts/Board/BoardNode.cs
@@ -1,11 +1,29 @@
 
+using System;
 using UnityEngine;
 
 public class BoardNode : MonoBehaviour, IGridNode
 {
     [SerializeField] private BoardPiece piece;
+
+    private Color gizmoColor;
+    private float gizmoSize;
+    public void Init(Color gizmoColor, float gizmoSize)
+    {
+        this.gizmoColor = gizmoColor;
+        this.gizmoSize = gizmoSize;
+    }
+    
     public bool IsOccupied()
     {
         return piece != null; 
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = gizmoColor;
+        
+        Vector3 cubeSize = new Vector3(gizmoSize, .01f, gizmoSize);
+        Gizmos.DrawCube(transform.position, cubeSize);
     }
 }
