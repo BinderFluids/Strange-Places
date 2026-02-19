3d0793b6e8d7dc6e5092ecc497790c888a0ab92c
Tanner Martin <79233634+BinderFluids@users.noreply.github.com>
2026-02-19 03:02:00 -0700

    Update BoardPlayer.cs
    Added control logic for selecting items

--- Assets/Scripts/Board/BoardPlayer.cs
+++ Assets/Scripts/Board/BoardPlayer.cs
@@ -1,8 +1,10 @@
 
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using EventBus;
 using Unity.VisualScripting;
+using UnityEditor.Rendering;
 using UnityEngine;
 
 public class BoardPlayer : MonoBehaviour, IPieceOwner
@@ -13,18 +15,48 @@ public class BoardPlayer : MonoBehaviour, IPieceOwner
     [SerializeField] private int actionsPerTurn = 2;
     [SerializeField] private int actionsAvailable = 2;
 
-    [SerializeField] private List<IBoardItem> items = new();
-    public List<IBoardItem> Items => items;
+    [SerializeField] private ItemSelectableHighlighter itemHighlighter;
+    [SerializeField] private List<BoardItem> items = new();
+    public List<BoardItem> Items => items;
+    EventBinding<SelectableChosenEvent> selectBinding;
 
     private Grid<BoardNode> workingGrid;
 
     private bool turnActive;
     public event Action onTurnEnd = delegate {};
 
+    private void Awake()
+    {
+        selectBinding = new EventBinding<SelectableChosenEvent>(OnSelectableChosenEvent);
+    }
+
+    
+
+    private Grid<BoardNode> ctx;
+    private bool clone; 
     public void StartTurn(Grid<BoardNode> ctx, bool clone = false)
     {
-        Debug.Log("starting turn");
-        
+        this.ctx = ctx;
+        this.clone = clone; 
+        if (items.Count > 0)
+        {
+            EventBus<SelectableChosenEvent>.Register(selectBinding);
+            SelectionManager.Instance
+                .StartSelection(
+                    items.Cast<ISelectable>().ToList(), 
+                    0,
+                    itemHighlighter
+                    ); 
+        }
+    }
+    void OnSelectableChosenEvent(SelectableChosenEvent boardNodeEvent)
+    {
+        EventBus<SelectableChosenEvent>.Deregister(selectBinding);
+        TurnActive(ctx, clone);
+    }
+
+    void TurnActive(Grid<BoardNode> ctx, bool clone = false)
+    {
         workingGrid = clone ? ctx.Copy() : ctx; 
         actionsAvailable = actionsPerTurn;
         turnActive = true; 
